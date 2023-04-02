c34cb5d898cbaf9b4743c922fccb6c3d585acd71
12210778-MaulanaAdityaPratama <12210778@bsi.ac.id>
2023-04-02 20:13:52 +0700

    project2

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pendataanwarga/views/views/login_views.dart';
+
+main(List<String> args) {
+  runApp(const MaterialApp(home: LoginView()));
+}
